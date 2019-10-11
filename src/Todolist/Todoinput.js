282b04d153e6653eebd64ea1ce073c36fa3c515b
HL1115 <1127570788@qq.com>
2019-10-11 14:41:08 +0800

    非受控组件

--- src/Todolist/Todoinput.js
+++ src/Todolist/Todoinput.js
@@ -4,16 +4,37 @@ import React, { Component } from 'react'
 
 // 受控组件和非受控组件
 
+// 非受控组件
+// 代码简单、比较适用于一次性获取表单的值
+// export default class Todoinput extends Component {
+//     componentDidMount(){
+//         console.log(this.inp.value);
+//         // console.log(this);
+//     }
+//     search = ()=>{
+//         console.log(this.inp.value);
+//     }
+//     render(){
+//         return (<div>
+//                     <input ref={inp=>this.inp = inp} type="text"/>
+//                     <button onClick={this.search}>查询</button>
+//                 </div>)
+//     }
+// }
+
+
+// 受控组件：value值被react的状态控制
+// 实时获取或处理输入的内容
 export default class Todoinput extends Component {
     constructor(){
         super();
         this.state = {
-            val: 'todo'
+            val: 0
         }
     }
     handleInput = (e)=>{
         if(e.keyCode === 13){
-            this.props.addTodo(e.target.value);
+            this.props.addTodo(this.state.val);
         }
     }
     handleChange = (e)=>{
@@ -25,6 +46,10 @@ export default class Todoinput extends Component {
         return (
             <div>
                 <input onChange={(e)=>this.handleChange(e)} value={this.state.val} onKeyDown={(e)=>this.handleInput(e)} type="text"/>
+                +
+                <input onChange={(e)=>this.handleChange(e)} value={this.state.val} onKeyDown={(e)=>this.handleInput(e)} type="text"/>
+                <p>0</p>
+                <button>查询</button>
             </div>
         )
     }
