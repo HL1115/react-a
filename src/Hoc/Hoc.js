6a763cca665f6a4fd679bdd0f3e70d00d47e9c17
HL1115 <1127570788@qq.com>
2019-10-15 11:07:02 +0800

    hoc

--- src/Hoc/Hoc.js
+++ src/Hoc/Hoc.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 let url = 'https://api.apiopen.top/musicRankingsDetails?type=1';
-function hoc(Com,url){
+function hoc(Com,url,title){
     class A extends Component{
         constructor(){
             super();
@@ -20,7 +20,12 @@ function hoc(Com,url){
             })
         }
         render(){
-            return <Com data={this.state.data} />
+            return (
+                <div>
+                    <h1>{title}</h1>
+                    <Com {...this.props} data={this.state.data} />
+                </div>
+            )
         }
     }
     return A;
@@ -28,6 +33,7 @@ function hoc(Com,url){
 class Music extends Component{
     render(){
         return <ul>
+            <div>{this.props.name}</div>
             {
                 this.props.data.map((item,idx)=>{
                     return <li key={idx}>{item.title}</li>
@@ -41,20 +47,24 @@ class Music1 extends Component{
         return <ul>
             {
                 this.props.data.map((item,idx)=>{
-                    return <p key={idx}>{item.title}</p>
+                    return (<div>
+                            <p key={idx}>{item.title}</p>
+                            <p key={idx}>{item.author}</p>
+                        </div>
+                    )
                 })
             }
         </ul>
     }
 }
-var Mymusic = hoc(Music,url);
-var Mymusic1 = hoc(Music1,url);
+var Mymusic = hoc(Music,url,'第一个');
+var Mymusic1 = hoc(Music1,url,'第二个');
 
 export default class Hoc extends Component {
     render() {
         return (
             <div>
-                <Mymusic/>
+                <Mymusic name='aa'/>
                 <Mymusic1/>
             </div>
         )
