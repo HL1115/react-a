96e12d3b5cc02242e8666aca4f4e9399d1f1426e
HL1115 <1127570788@qq.com>
2019-10-23 08:32:27 +0800

    req

--- src/Router/Topic.js
+++ src/Router/Topic.js
@@ -18,20 +18,22 @@ export default class Topic extends Component {
     // shouldComponentUpdate(){
         
     // }
-    componentDidUpdate(){
-        let page = this.props.match.params.page;
-        fetch('https://cnodejs.org/api/v1/topics?page='+page)
-            .then((res)=>res.json())
-            .then((res)=>{
-                this.setState({data:res.data});
-            })
+    componentDidUpdate(prevProps,prevState){
+        if(prevProps.match.params.page!==this.props.match.params.page){
+            let page = this.props.match.params.page;
+            fetch('https://cnodejs.org/api/v1/topics?page='+page)
+                .then((res)=>res.json())
+                .then((res)=>{
+                        this.setState({data:res.data});
+                    })
+        }
     }
     render() {
         return (
             <div>
                 {
                     this.state.data.map((item)=>(
-                        <div style={{width:800,height:800,overflow:'hidden'}} dangerouslySetInnerHTML={{__html:item.content}}></div>
+                        <div style={{width:800,overflow:'hidden'}} dangerouslySetInnerHTML={{__html:item.title}}></div>
                     ))
                 }
             </div>
