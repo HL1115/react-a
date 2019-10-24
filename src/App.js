c2936e02e6e531d13dfaee931e9875d78b3280ae
HL1115 <1127570788@qq.com>
2019-10-24 08:46:00 +0800

    main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
-
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                <Header/>
-            </div>
+            <Router>
+                <div>
+                    <Header/>
+                    <div className='main'>
+                        <div className="content">
+                            <Route />
+                        </div>
+                        <div className="sider">
+
+                        </div>
+                    </div>
+                </div>
+            </Router>
         )
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function Header(){
 
@@ -6,7 +7,14 @@ export default function Header(){
         <div className='headerwrap'>
             <header>
                 <img src="https://static2.cnodejs.org/public/images/cnodejs_light.svg" alt=""/>
-                
+                <div style={{float:'right',lineHeight:'50px'}}>
+                    <Link>首页</Link>
+                    <Link>新手入门</Link>
+                    <Link>API</Link>
+                    <Link>关于</Link>
+                    <Link>注册</Link>
+                    <Link>登录</Link>
+                </div>
             </header>
         </div>
     )
