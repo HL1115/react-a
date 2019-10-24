58dd9aba7f6d63211893358e024bbbcfd40d100d
HL1115 <1127570788@qq.com>
2019-10-24 09:00:36 +0800

    com

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
 import {BrowserRouter as Router,Route} from 'react-router-dom';
+import Home from './container/Home';
+import Api from './container/Api';
+import About from './container/About';
+import Start from './container/Start';
 
 export default class App extends Component {
     render() {
@@ -10,7 +14,10 @@ export default class App extends Component {
                     <Header/>
                     <div className='main'>
                         <div className="content">
-                            <Route />
+                            <Route path='/home' component={Home} />
+                            <Route path='/api' component={Api} />
+                            <Route path='/start' component={Start} />
+                            <Route path='/about' component={About} />
                         </div>
                         <div className="sider">
 
