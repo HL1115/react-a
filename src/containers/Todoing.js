b2c610fe409964c90dd9df18795a0237836a3f29
HL1115 <1127570788@qq.com>
2019-10-31 10:51:36 +0800

    todoing

--- src/containers/Todoing.js
+++ src/containers/Todoing.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react'
-import store from '../store';
 import {delItem} from '../actions/actionCreators';
 import {connect} from 'react-redux';
-class Todoing extends Component {
-    delItem = (index)=>{
-        this.props.dispatch(delItem(index))
+
+// class Todoing extends Component {
+//     delItem = (index)=>{
+//         this.props.dispatch(delItem(index))
+//     }
+//     render() {
+//         return (
+//             <ul>
+//                 {
+//                     this.props.todos.map((item,idx)=>(
+//                         <li key={item}>{item}---<button onClick={()=>this.delItem(idx)}>删除</button></li>
+//                     ))
+//                 } 
+//             </ul>
+//         )
+//     }
+// }
+function Todoing({dispatch,todos}){
+    let del = (index)=>{
+        dispatch(delItem(index))
     }
-    render() {
-        return (
-            <ul>
-                {
-                    this.props.todos.map((item,idx)=>(
-                        <li key={item}>{item}---<button onClick={()=>this.delItem(idx)}>删除</button></li>
-                    ))
-                } 
-            </ul>
-        )
-    }
+    return (
+        <ul>
+            {
+                todos.map((item,idx)=>(
+                    <li key={item}>{item}---<button onClick={()=>del(idx)}>删除</button></li>
+                ))
+            } 
+        </ul>
+    )
 }
+
 let mapStateToProps = (state)=>{
     return {
         todos: state.todo
